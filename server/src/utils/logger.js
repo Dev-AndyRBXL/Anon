044aa1984840c6e040f74537eef084f4a0e846fc
Dev_Andy <andrealeman.contact@gmail.com>
2025-09-08 23:50:12 -0600

    Update logger.js

--- server/src/utils/logger.js
+++ server/src/utils/logger.js
@@ -1,6 +1,7 @@
 const pino = require('pino');
 const config = require('../config');
 
+// Use pino-pretty ONLY during development
 const logger = pino({
   transport: {
     target: config.isDevelopment() ? 'pino-pretty' : 'pino/file',
