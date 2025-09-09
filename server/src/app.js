e7879e63475397ba985a70a5de5b6992eae1e27a
Dev_Andy <andrealeman.contact@gmail.com>
2025-09-09 14:49:13 -0600

    Update 'Template'

--- server/src/app.js
+++ server/src/app.js
@@ -9,21 +9,25 @@ const app = express();
 
 app.use(express.json());
 app.use(passport.initialize());
-app.use(cors({
-  origin: 'http://localhost:5173',
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    credentials: true,
+  })
+);
 
-/**
- * Template of request:
- * Headers:
- *   x-api-key: [api key]
- *
- * Body:
+/** Template
+ * POST /endpoint HTTP/1.1
+ * Host: example.com
+ * x-api-key: [your-api-key]
+ * Authorization: Bearer [your-jwt-token]
+ * Content-Type: application/json
+
  * {
- *   ...[other stuff]
+ * ...[other data]
  * }
+ * 
  */
 
 app.use('/api/auth', validateApiKey, authRoutes);
