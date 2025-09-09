8cb9f561a23b06879f16455d333d36af6c389011
Dev_Andy <andrealeman.contact@gmail.com>
2025-09-09 14:09:04 -0600

    Update schemas.js

--- server/src/utils/schemas.js
+++ server/src/utils/schemas.js
@@ -8,12 +8,13 @@ exports.userSchema = {
   displayname: { type: DataTypes.STRING },
   username: { type: DataTypes.STRING, unique: true, allowNull: false },
   password: { type: DataTypes.STRING, allowNull: false },
-  description: { type: DataTypes.TEXT, allowNull: true },
+  description: { type: DataTypes.TEXT },
   email: {
     type: DataTypes.STRING,
     unique: true,
     allowNull: false,
-    validate: { isEmail: true }, // removed stray "z"
+    validate: { isEmail: true }, 
   },
   role: { type: DataTypes.STRING, allowNull: false, defaultValue: 'user' },
+  createdAt: { type: DataTypes.DATE },
 };
