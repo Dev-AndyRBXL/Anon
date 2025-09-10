1097f2d39eba58c8bede56dcf47a679d7f35503b
Dev_Andy <andrealeman.contact@gmail.com>
2025-09-09 20:29:11 -0600

    Add eslint

--- app/eslint.config.js
+++ app/eslint.config.js
@@ -1,23 +1,37 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
-
-export default tseslint.config([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+export default {
+  ignorePatterns: ['dist'],
+  overrides: [
+    {
+      files: ['**/*.{ts,tsx}'],
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:react-refresh/recommended',
+      ],
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+      env: {
+        browser: true,
+      },
+      plugins: ['@typescript-eslint', 'react-hooks', 'react-refresh', 'import'],
+      rules: {
+        'import/no-restricted-paths': [
+          'error',
+          {
+            zones: [
+              {
+                target: './src',          
+                from: './src/features',  
+                message:
+                  'You cannot import anything from the features folder outside of it.',
+              },
+            ],
+          },
+        ],
+      },
     },
-  },
-])
+  ],
+};
